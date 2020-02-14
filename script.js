e5ffd5d3449bf8066f89ac169e9f44969197803b
ogbeche77 <ogbeche77@yahoo.com>
2020-02-14 16:03:56 +0100

    style added

---
+++ script.js
@@ -0,0 +1,14 @@
+//selecting element
+
+const cartBtn = document.querySelector(".cart-btn");
+const CloseCartBtn = document.querySelector(".close-cart");
+const ClearCartBtn = document.querySelector(".clear-cart");
+const cartDOM = document.querySelector(".cart");
+const cartOverlay = document.querySelector(".cart-overlay");
+const cartItems = document.querySelector(".cart-items");
+const cartTotal = document.querySelector(".cart-total");
+const cartContent = document.querySelector(".cart-content");
+const productsDOM = document.querySelector(".products-center");
+
+
+let cart = []
