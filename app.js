b1bef0422454cc188f9fdb5d8d9ebdc1c9610137
ogbeche77 <ogbeche77@yahoo.com>
2020-04-01 12:42:17 +0200

    spacing

--- app.js
+++ app.js
@@ -7,37 +7,31 @@ const passport = require("passport");
 const path = require("path");
 require("dotenv").config();
 
-
-
-
 const app = express();
 
 //passport configuration
 require("./config/passport")(passport);
 
-
 //Database configuration
 const db = require("./config/key").MongoURI;
 
 // Connect to Mongo
-mongoose.connect(db, {useUnifiedTopology: true, useNewUrlParser: true})
-.then(()=> console.log("MongoDB is still connected"))
-.catch(err => console.log(err));
+mongoose
+  .connect(db, { useUnifiedTopology: true, useNewUrlParser: true })
+  .then(() => console.log("MongoDB is still connected"))
+  .catch(err => console.log(err));
 
 /*app.use(express.static("public"));*/
-
 
 //EJS
 app.use(expressLayouts);
 app.set("view engine", "ejs");
 
-
 app.use(express.static("public"));
 
-    app.get("/", (req, res) => {
-        res.sendFile(path.resolve(__dirname, "public", "index.html"));
-
-    });
+app.get("/", (req, res) => {
+  res.sendFile(path.resolve(__dirname, "public", "index.html"));
+});
 
 /*
 app.get("/", function (req, res){
@@ -45,35 +39,32 @@ app.get("/", function (req, res){
 })
 */
 
-
 //add bodyparser
-app.use(express.urlencoded ({ extended: false}));
+app.use(express.urlencoded({ extended: false }));
 
 // Express session middleware (gotten from github express session readme page)
-app.use(session({
-    secret: 'secret',
+app.use(
+  session({
+    secret: "secret",
     resave: false,
     saveUninitialized: true
-  }));
+  })
+);
 
 //PassportJS middleware
 app.use(passport.initialize());
 app.use(passport.session());
 
-
-
-
-  // Connect flash messaging
-  app.use(flash());
+// Connect flash messaging
+app.use(flash());
 
 //Global Variables
-app.use((req, res, next)=>{
-    res.locals.success_msg = req.flash("success_msg");
-    res.locals.error_msg = req.flash("error_msg");
-    res.locals.error = req.flash("error");
-    next();
+app.use((req, res, next) => {
+  res.locals.success_msg = req.flash("success_msg");
+  res.locals.error_msg = req.flash("error_msg");
+  res.locals.error = req.flash("error");
+  next();
 });
-
 
 //Routes
 app.use("/", require("./routes/front"));
